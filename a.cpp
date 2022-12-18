78330e041643a1fce552b6ad53cbcba2900fd1cf
Muhammad Jilani <120803991+jilani192@users.noreply.github.com>
2022-12-18 16:14:25 +0500

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    cout << "My name is Muhammad Jilani. " << endl;
+    cout << "My roll number is 192. ";
+}
---
+++ c.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<"    .....                        ooooo     ooooo              ...........       "<<endl;                                          
+cout<<"    ......                      o     o   o     o             ..........        "<<endl;        
+cout<<"    .......                        ooo    o     o             .........         "<<endl;        
+cout<<"    ........                     oo       o     o             ........          "<<endl;       
+cout<<"     ........                   ooooooo    ooooo  th          .......           "<<endl;           
+cout<<"       .......                .........................       ......            "<<endl;              
+cout<<"         ......                     C E N T U R Y             .....             "<<endl;                       
+cout<<"           .....              .........................       ....              "<<endl;                        
+cout<<"              ...             ooooo  ooooo  o   o ooooo       ...               "<<endl;                        
+cout<<"                oo              o    o       o o    o         oo                "<<endl;         
+cout<<"               _||_             o    oooo     o     o        _||_               "<<endl;                 
+cout<<"              |    |            o    o       o o    o       |    |              "<<endl;      
+cout<<"    __________|____|___________ o    ooooo  o   o   o ______|____|______        "<<endl;                    
+}
