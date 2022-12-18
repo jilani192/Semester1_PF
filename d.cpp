aeb0afec37dba03f114c185456e99965c7502efb
Muhammad Jilani <120803991+jilani192@users.noreply.github.com>
2022-12-18 19:07:47 +0500

    Add files via upload

---
+++ d.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "       ##          #     #     ########    ooo        ooo     ###     ###                                                                                  " << endl;
+    cout << "        ##          #   #      #     #      oo     oo          ##     ##                                                             " << endl;
+    cout << "      ## ##          # #           #        oo   oo            ##     ##                                                           " << endl;
+    cout << "      ##  ##          #          #    #     oo oo              ##     ##                                                          " << endl;
+    cout << "      ######         # #       ########     oo   oo            ##     ##                                                                 " << endl;
+    cout << "      ##  ##        #   #                   oo     oo           ## ## #                                                                      " << endl;
+    cout << "     ###  ##       #     #                 oooo       ooo                                                     " << endl;
+    cout << "                                                                                                                       " << endl;
+    cout << "                                                                " << endl;
+}
---
+++ f.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    system("Color 76");
+    cout << "       oooooooooo  ooo    ooooo                                                 " << endl;
+    cout << "      oo     oo   oo oo   oo  oo                                          " << endl;
+    cout << "       oooo  oo  oo   oo  ooooo                                              " << endl;
+    cout << "          oo oo ooooooooo oo   oo                                               " << endl;
+    cout << "   oooooooo  oo oo     oo oo    oooooo                                                 " << endl;
+    cout << "                                                              " << endl;
+    cout << "   oo  oo  oo   ooo    ooooo    oooooo                                            " << endl;
+    cout << "   oo  oo  oo  oo oo   oo  oo  oo                                             " << endl;
+    cout << "   oo oooo oo oo   oo  ooooo    oooo                                               " << endl;
+    cout << "    ooo  ooo ooooooooo oo   oo     oo                                              " << endl;
+    cout << "     oo  oo  oo     oo oo    ooooooo                                            " << endl;
+    cout << "                                                              " << endl;
+}
